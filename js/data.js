f6f7e015ae30a70b20882723e37a62b1b53d39f7
Bagas  Pratama <bgsprtm56@gmail.com>
2024-05-15 20:26:31 +0700

    commit

---
+++ js/data.js
@@ -0,0 +1,48 @@
+export const mapClasses =[
+    "variant-1",
+    "variant-2",
+    "variant-3",
+    "variant-1",
+    "variant-2",
+    "variant-3",
+    "variant-1"
+];
+
+export const previews = [
+    {
+        img: "./assets/bg-1.jpg",
+        title: "Nama",
+        tags: "Nama Gua Bagas Pratama",
+        description: "Gua anak pertama dari 4 bersaudara, gua lahir 4 april 2001, ohh iya zodiak gua aries yang kalem ye"
+
+    },{
+        img:"./assets/bg-2.jpg",
+        title:"umur",
+        tags:"Umur gua 23 tahun",
+        description:"Di Umur yang ke 23 ini gua berharap bisa bergabung menjadi part of cretivox batch spesial edtion"
+    },{
+        img:"./assets/bg-3.jpg",
+        title:"Kuliah dimana?",
+        tags:"Kuliah Gua Di Universitas Sebelas Maret",
+        description:"Btw gua fresh graduate dari kampus UNS ohh iya prodi gua D3 Teknik Informatika"
+    },{
+        img:"./assets/bg-4.jpg",
+        title:"Divisi",
+        tags:"Divisi yang gua ambil itu FrontEnd Developer",
+        description:"Gua juga ada pengalaman menjadi frontend sekitar 6 bulan.andaikan lolos dicretivox ini,semoga gua bisa berkembang bersama rekan rekan semua "
+    },{
+        img:"./assets/bg-5.jpg",
+        title:"HT",
+        tags:"jago nyari link streaming",
+        description:" gua anak kost yang dimana ga ada tv. itulah saatnya gua beraksi dan temen temen gua selalu minta infoin linknya"
+    },{
+        img:"./assets/bg-6.jpg",
+        title:"harus diterima?",
+        tags:"Ga harus si intinya gua udah berusaha yaps nothing to lose aja",
+        description:"Btw gua pengen banget di umur yang sekarang kenal dengan temen temen yang berjuang buat dirinya,goals kita sama brooo"
+    },
+    {
+        img:"./assets/bg-7.jpg"
+    }
+    
+];
